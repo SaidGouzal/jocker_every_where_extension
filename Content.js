49b61aaec99d2f1f06a3c60156c14aa67dabd713
Said Gouzal (Eswaad) <gouzal.said3@gmail.com>
2022-04-24 02:21:36 +0000

    remove audio

--- Content.js
+++ Content.js
@@ -1,13 +1,8 @@
 const url = "https://cdn.vox-cdn.com/thumbor/WAVESAmwUdxWYZKuUr48zpid3nA=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/19187243/joker2.jpg";
-
 const images = document.getElementsByTagName("img");
 setInterval(() => {
   for (let i = 0; i < images.length; i++) {
     const element = images[i];
     element.src = url
   }
-
-  var audio = new Audio('Joker_laughing.mp3');
-  audio.play();
-
-}, 4000);
+}, 2000);
